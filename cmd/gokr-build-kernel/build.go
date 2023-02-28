ff8cd165fd5973585b3cc9462e5a0c1a24730b97
Michael Stapelberg <michael@stapelberg.de>
2023-02-28 20:35:44 +0100

    Revert "auto-update to linux-6.2.tar.xz (#491)"
    This reverts commit ce9b7ea9d6d375a55b7bbc26ce641cfa836db2df.
    
    breaks WiFi on the Raspberry Pi 3B+ (possibly others)
    
    related to https://github.com/gokrazy/wifi/issues/3

--- cmd/gokr-build-kernel/build.go
+++ cmd/gokr-build-kernel/build.go
@@ -14,7 +14,7 @@ import (
 )
 
 // see https://www.kernel.org/releases.json
-var latest = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.2.tar.xz"
+var latest = "https://cdn.kernel.org/pub/linux/kernel/v6.x/linux-6.1.12.tar.xz"
 
 const configAddendum = `
 CONFIG_ARCH_BCM2835=y 
